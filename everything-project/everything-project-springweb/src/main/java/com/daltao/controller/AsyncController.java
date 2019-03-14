f2a02e81b7c3f054e5f62a3830546bdad2374320
taodaling <daling.tao@ideacome.com>
2019-03-14 09:36:45 +0800

    Async test

--- everything-project/everything-project-springweb/src/main/java/com/daltao/controller/AsyncController.java
+++ everything-project/everything-project-springweb/src/main/java/com/daltao/controller/AsyncController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/async")
@@ -13,8 +14,7 @@ public class AsyncController {
     AsyncService asyncService;
 
     @RequestMapping("/invoke")
-    public String invoke(){
-        asyncService.asyncInvoke();
-        return "ok";
+    public String invoke() throws ExecutionException, InterruptedException {
+        return asyncService.asyncInvoke().get().toString();
     }
 }
--- everything-project/everything-project-springweb/src/main/java/com/daltao/service/async/AsyncService.java
+++ everything-project/everything-project-springweb/src/main/java/com/daltao/service/async/AsyncService.java
@@ -1,19 +1,24 @@
 package com.daltao.service.async;
 
+import com.google.common.util.concurrent.Futures;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.concurrent.Future;
 
 @Service
 @EnableAsync
 public class AsyncService {
     @Async
-    public void asyncInvoke(){
+    public Future<Date> asyncInvoke() {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        return Futures.immediateFuture(new Date());
     }
 }
 
