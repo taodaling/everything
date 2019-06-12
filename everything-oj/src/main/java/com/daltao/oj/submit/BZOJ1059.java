5a837b20c553116a5b9401014aecc302c74cd48f
taodaling <daling.tao@ideacome.com>
2019-06-12 18:05:09 +0800

    BZOJ搞起

---
+++ everything-oj/src/main/java/com/daltao/oj/submit/BZOJ1059.java
@@ -0,0 +1,502 @@
+package com.daltao.oj.submit;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+public class BZOJ1059 {
+    public static void main(String[] args) throws Exception {
+        boolean local = System.getProperty("ONLINE_JUDGE") == null;
+        boolean async = false;
+
+        Charset charset = Charset.forName("ascii");
+
+        FastIO io = local ? new FastIO(new FileInputStream("D:\\DATABASE\\TESTCASE\\Code.in"), System.out, charset) : new FastIO(System.in, System.out, charset);
+        Task task = new Task(io, new Debug(local));
+
+        if (async) {
+            Thread t = new Thread(null, task, "dalt", 1 << 27);
+            t.setPriority(Thread.MAX_PRIORITY);
+            t.start();
+            t.join();
+        } else {
+            task.run();
+        }
+
+        if (local) {
+            io.cache.append("\n\n--memory -- \n" + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) >> 20) + "M");
+        }
+
+        io.flush();
+    }
+
+    public static class Task implements Runnable {
+        final FastIO io;
+        final Debug debug;
+        int inf = (int) 1e8;
+
+        public Task(FastIO io, Debug debug) {
+            this.io = io;
+            this.debug = debug;
+        }
+
+        @Override
+        public void run() {
+            int t = io.readInt();
+            while (t-- > 0)
+                solve();
+        }
+
+        Matrix matrix = new Matrix(200, 200);
+
+        public void solve() {
+            int n = matrix.n = matrix.m = io.readInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+
+                }
+            }
+        }
+    }
+
+
+    public static class Matrix implements Cloneable {
+        double[][] mat;
+        int n;
+        int m;
+
+        public Matrix(Matrix model) {
+            n = model.n;
+            m = model.m;
+            mat = new double[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = model.mat[i][j];
+                }
+            }
+        }
+
+        public Matrix(int n, int m) {
+            this.n = n;
+            this.m = m;
+            mat = new double[n][m];
+        }
+
+        public void fill(int v) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = v;
+                }
+            }
+        }
+
+        public void asStandard() {
+            fill(0);
+            for (int i = 0; i < n && i < m; i++) {
+                mat[i][i] = 1;
+            }
+        }
+
+        public static Matrix mul(Matrix a, Matrix b) {
+            Matrix c = new Matrix(a.n, b.m);
+            for (int i = 0; i < c.n; i++) {
+                for (int j = 0; j < c.m; j++) {
+                    for (int k = 0; k < a.m; k++) {
+                        c.mat[i][j] = c.mat[i][j] + a.mat[i][k] * b.mat[k][j];
+                    }
+                }
+            }
+            return c;
+        }
+
+        public static Matrix pow(Matrix x, int n) {
+            if (n == 0) {
+                Matrix r = new Matrix(x.n, x.m);
+                r.asStandard();
+                return r;
+            }
+            Matrix r = pow(x, n >> 1);
+            r = Matrix.mul(r, r);
+            if (n % 2 == 1) {
+                r = Matrix.mul(r, x);
+            }
+            return r;
+        }
+
+        public static Matrix inverse(Matrix x) {
+            if (x.n != x.m) {
+                throw new RuntimeException("Matrix is not square");
+            }
+            int n = x.n;
+            Matrix l = new Matrix(x);
+            Matrix r = new Matrix(n, n);
+            r.asStandard();
+            for (int i = 0; i < n; i++) {
+                int maxRow = i;
+                for (int j = i; j < n; j++) {
+                    if (Math.abs(l.mat[j][i]) > Math.abs(l.mat[maxRow][i])) {
+                        maxRow = j;
+                    }
+                }
+
+                if (l.mat[maxRow][i] == 0) {
+                    throw new RuntimeException("Can't inverse current matrix");
+                }
+                r.swapRow(i, maxRow);
+                l.swapRow(i, maxRow);
+
+                r.divideRow(i, l.mat[i][i]);
+                l.divideRow(i, l.mat[i][i]);
+
+                for (int j = 0; j < n; j++) {
+                    if (j == i) {
+                        continue;
+                    }
+                    if (l.mat[j][i] == 0) {
+                        continue;
+                    }
+                    double f = l.mat[j][i];
+                    r.subtractRow(j, i, f);
+                    l.subtractRow(j, i, f);
+                }
+            }
+            return r;
+        }
+
+        static Matrix transposition(Matrix x) {
+            int n = x.n;
+            int m = x.m;
+            Matrix t = new Matrix(m, n);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    t.mat[j][i] = x.mat[i][j];
+                }
+            }
+            return t;
+        }
+
+        void swapRow(int i, int j) {
+            double[] row = mat[i];
+            mat[i] = mat[j];
+            mat[j] = row;
+        }
+
+        void subtractRow(int i, int j, double f) {
+            for (int k = 0; k < m; k++) {
+                mat[i][k] -= mat[j][k] * f;
+            }
+        }
+
+        void divideRow(int i, double f) {
+            for (int k = 0; k < m; k++) {
+                mat[i][k] /= f;
+            }
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    builder.append(mat[i][j]).append(' ');
+                }
+                builder.append('\n');
+            }
+            return builder.toString();
+        }
+
+    }
+
+    public static class FastIO {
+        public final StringBuilder cache = new StringBuilder();
+        private final InputStream is;
+        private final OutputStream os;
+        private final Charset charset;
+        private StringBuilder defaultStringBuf = new StringBuilder(1 << 8);
+        private byte[] buf = new byte[1 << 13];
+        private int bufLen;
+        private int bufOffset;
+        private int next;
+
+        public FastIO(InputStream is, OutputStream os, Charset charset) {
+            this.is = is;
+            this.os = os;
+            this.charset = charset;
+        }
+
+        public FastIO(InputStream is, OutputStream os) {
+            this(is, os, Charset.forName("ascii"));
+        }
+
+        private int read() {
+            while (bufLen == bufOffset) {
+                bufOffset = 0;
+                try {
+                    bufLen = is.read(buf);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                if (bufLen == -1) {
+                    return -1;
+                }
+            }
+            return buf[bufOffset++];
+        }
+
+        public void skipBlank() {
+            while (next >= 0 && next <= 32) {
+                next = read();
+            }
+        }
+
+        public int readInt() {
+            int sign = 1;
+
+            skipBlank();
+            if (next == '+' || next == '-') {
+                sign = next == '+' ? 1 : -1;
+                next = read();
+            }
+
+            int val = 0;
+            if (sign == 1) {
+                while (next >= '0' && next <= '9') {
+                    val = val * 10 + next - '0';
+                    next = read();
+                }
+            } else {
+                while (next >= '0' && next <= '9') {
+                    val = val * 10 - next + '0';
+                    next = read();
+                }
+            }
+
+            return val;
+        }
+
+        public long readLong() {
+            int sign = 1;
+
+            skipBlank();
+            if (next == '+' || next == '-') {
+                sign = next == '+' ? 1 : -1;
+                next = read();
+            }
+
+            long val = 0;
+            if (sign == 1) {
+                while (next >= '0' && next <= '9') {
+                    val = val * 10 + next - '0';
+                    next = read();
+                }
+            } else {
+                while (next >= '0' && next <= '9') {
+                    val = val * 10 - next + '0';
+                    next = read();
+                }
+            }
+
+            return val;
+        }
+
+        public double readDouble() {
+            boolean sign = true;
+            skipBlank();
+            if (next == '+' || next == '-') {
+                sign = next == '+';
+                next = read();
+            }
+
+            long val = 0;
+            while (next >= '0' && next <= '9') {
+                val = val * 10 + next - '0';
+                next = read();
+            }
+            if (next != '.') {
+                return sign ? val : -val;
+            }
+            next = read();
+            long radix = 1;
+            long point = 0;
+            while (next >= '0' && next <= '9') {
+                point = point * 10 + next - '0';
+                radix = radix * 10;
+                next = read();
+            }
+            double result = val + (double) point / radix;
+            return sign ? result : -result;
+        }
+
+        public String readString(StringBuilder builder) {
+            skipBlank();
+
+            while (next > 32) {
+                builder.append((char) next);
+                next = read();
+            }
+
+            return builder.toString();
+        }
+
+        public String readString() {
+            defaultStringBuf.setLength(0);
+            return readString(defaultStringBuf);
+        }
+
+        public int readLine(char[] data, int offset) {
+            int originalOffset = offset;
+            while (next != -1 && next != '\n') {
+                data[offset++] = (char) next;
+                next = read();
+            }
+            return offset - originalOffset;
+        }
+
+        public int readString(char[] data, int offset) {
+            skipBlank();
+
+            int originalOffset = offset;
+            while (next > 32) {
+                data[offset++] = (char) next;
+                next = read();
+            }
+
+            return offset - originalOffset;
+        }
+
+        public int readString(byte[] data, int offset) {
+            skipBlank();
+
+            int originalOffset = offset;
+            while (next > 32) {
+                data[offset++] = (byte) next;
+                next = read();
+            }
+
+            return offset - originalOffset;
+        }
+
+        public char readChar() {
+            skipBlank();
+            char c = (char) next;
+            next = read();
+            return c;
+        }
+
+        public void flush() {
+            try {
+                os.write(cache.toString().getBytes(charset));
+                os.flush();
+                cache.setLength(0);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public boolean hasMore() {
+            skipBlank();
+            return next != -1;
+        }
+    }
+
+    public static class Debug {
+        private boolean allowDebug;
+
+        public Debug(boolean allowDebug) {
+            this.allowDebug = allowDebug;
+        }
+
+        public void assertTrue(boolean flag) {
+            if (!allowDebug) {
+                return;
+            }
+            if (!flag) {
+                fail();
+            }
+        }
+
+        public void fail() {
+            throw new RuntimeException();
+        }
+
+        public void assertFalse(boolean flag) {
+            if (!allowDebug) {
+                return;
+            }
+            if (flag) {
+                fail();
+            }
+        }
+
+        private void outputName(String name) {
+            System.out.print(name + " = ");
+        }
+
+        public void debug(String name, int x) {
+            if (!allowDebug) {
+                return;
+            }
+
+            outputName(name);
+            System.out.println("" + x);
+        }
+
+        public void debug(String name, long x) {
+            if (!allowDebug) {
+                return;
+            }
+            outputName(name);
+            System.out.println("" + x);
+        }
+
+        public void debug(String name, double x) {
+            if (!allowDebug) {
+                return;
+            }
+            outputName(name);
+            System.out.println("" + x);
+        }
+
+        public void debug(String name, int[] x) {
+            if (!allowDebug) {
+                return;
+            }
+            outputName(name);
+            System.out.println(Arrays.toString(x));
+        }
+
+        public void debug(String name, long[] x) {
+            if (!allowDebug) {
+                return;
+            }
+            outputName(name);
+            System.out.println(Arrays.toString(x));
+        }
+
+        public void debug(String name, double[] x) {
+            if (!allowDebug) {
+                return;
+            }
+            outputName(name);
+            System.out.println(Arrays.toString(x));
+        }
+
+        public void debug(String name, Object x) {
+            if (!allowDebug) {
+                return;
+            }
+            outputName(name);
+            System.out.println("" + x);
+        }
+
+        public void debug(String name, Object... x) {
+            if (!allowDebug) {
+                return;
+            }
+            outputName(name);
+            System.out.println(Arrays.deepToString(x));
+        }
+    }
+}
