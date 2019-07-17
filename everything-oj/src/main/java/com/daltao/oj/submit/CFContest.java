9cca6021a44f6c5c1547d34ebabc46e4c2908331
taodaling <taodaling@gmail.com>
2019-07-18 00:44:22 +0800

    P.F

--- everything-oj/src/main/java/com/daltao/oj/submit/CFContest.java
+++ everything-oj/src/main/java/com/daltao/oj/submit/CFContest.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 
 public class CFContest {
@@ -49,446 +46,211 @@ public class CFContest {
 
         @Override
         public void run() {
-            int t = io.readInt();
-            while (t-- > 0)
-                solve();
+            solve();
         }
 
-
-        NumberTheory.Modular mod = new NumberTheory.Modular(0);
-        NumberTheory.QuadraticResidue qr = new NumberTheory.QuadraticResidue(mod);
-
         public void solve() {
-            mod.m = io.readInt();
-            io.cache.append(qr.minPrimitiveRoot()).append('\n');
+            int n = io.readInt();
+            List<Line> lines = new ArrayList<>();
+            lines.add(null);
+            lines.add(null);
+            Convex[] convexes = new Convex[n + 1];
+            int[] last = new int[2];
+            int[] now = new int[2];
+            for (int i = 1; i <= n; i++) {
+                int num = io.readInt();
+                convexes[i] = new Convex();
+                convexes[i].from = lines.size();
+                convexes[i].to = convexes[i].from + num;
+
+                int firstX = io.readInt();
+                int firstY = io.readInt();
+                last[0] = firstX;
+                last[1] = firstY;
+                for (int j = 1; j < num; j++) {
+                    now[0] = io.readInt();
+                    now[1] = io.readInt();
+                    lines.add(new Line(now[0] - last[0], now[1] - last[1]));
+                    int[] tmp = now;
+                    now = last;
+                    last = tmp;
+                }
+                lines.add(new Line(firstX - last[0], firstY - last[1]));
+            }
+
+            TreeMap<Line, Integer> registries = new TreeMap<>(Line.sortByAngle);
+            int m = lines.size();
+            for (int i = 2; i < m; i++) {
+                Line line = lines.get(i);
+                line.val = registries.getOrDefault(line, 1);
+                registries.put(line, i);
+            }
+
+            BIT bit = new BIT(m);
+            int q = io.readInt();
+            Query[] queries = new Query[q];
+            for (int i = 0; i < q; i++) {
+                queries[i] = new Query();
+                int a = io.readInt();
+                int b = io.readInt();
+                queries[i].l = convexes[a].from;
+                queries[i].r = convexes[b].to;
+            }
+
+            Query[] queriesSortByR = queries.clone();
+            Arrays.sort(queriesSortByR, (a, b) -> a.r - b.r);
+
+            int rpos = 2;
+            for (Query query : queriesSortByR) {
+                while (rpos < query.r) {
+                    bit.update(lines.get(rpos).val, 1);
+                    rpos++;
+                }
+                int ans = bit.query(query.l - 1) - (query.l - 2);
+                query.ans = ans;
+            }
+
+            for (Query query : queries) {
+                io.cache.append(query.ans).append('\n');
+            }
         }
     }
 
-    public static class NumberTheory {
-        private static final Random RANDOM = new Random();
+    public static class Query {
+        int l;
+        int r;
+        int ans;
+    }
 
+    public static class Convex {
+        int from;
+        int to;
+    }
 
-        /**
-         * Extend gcd
-         */
-        public static class ExtGCD {
-            private long x;
-            private long y;
-            private long g;
+    public static class Line {
+        long x;
+        long y;
+        int val;
+        final double theta;
 
-            public long getX() {
-                return x;
-            }
-
-            public long getY() {
-                return y;
-            }
-
-            /**
-             * Get g = Gcd(a, b) and find a way to set x and y to match ax+by=g
-             */
-            public long extgcd(long a, long b) {
-                if (a >= b) {
-                    g = extgcd0(a, b);
-                } else {
-                    g = extgcd0(b, a);
-                    long tmp = x;
-                    x = y;
-                    y = tmp;
-                }
-                return g;
-            }
-
-
-            private long extgcd0(long a, long b) {
-                if (b == 0) {
-                    x = 1;
-                    y = 0;
-                    return a;
-                }
-                long g = extgcd0(b, a % b);
-                long n = x;
-                long m = y;
-                x = m;
-                y = n - m * (a / b);
-                return g;
-            }
+        Line(long x, long y) {
+            this.x = x;
+            this.y = y;
+            theta = Math.atan2(y, x);
         }
 
-        public static class Gcd {
-            public long gcd(long a, long b) {
-                return a >= b ? gcd0(a, b) : gcd0(b, a);
-            }
-
-            private long gcd0(long a, long b) {
-                return b == 0 ? a : gcd0(b, a % b);
-            }
-
-            public int gcd(int a, int b) {
-                return a >= b ? gcd0(a, b) : gcd0(b, a);
-            }
-
-            private int gcd0(int a, int b) {
-                return b == 0 ? a : gcd0(b, a % b);
-            }
+        public static long cross(long x1, long y1, long x2, long y2) {
+            return x1 * y2 - y1 * x2;
         }
 
+        public static Comparator<Line> sortByAngle = (a, b) -> {
+            return Math.abs(a.theta - b.theta) < 1e-10 ? 0 : Double.compare(a.theta, b.theta);
+        };
+    }
+
+
+    /**
+     * Created by dalt on 2018/5/20.
+     */
+    public static class BIT {
+        private int[] data;
+        private int n;
 
         /**
-         * Mod operations
+         * 创建大小A[1...n]
          */
-        public static class Modular {
-            int m;
+        public BIT(int n) {
+            this.n = n;
+            data = new int[n + 1];
+        }
 
-            public Modular(int m) {
-                this.m = m;
+        /**
+         * 查询A[1]+A[2]+...+A[i]
+         */
+        public int query(int i) {
+            int sum = 0;
+            for (; i > 0; i -= i & -i) {
+                sum += data[i];
             }
+            return sum;
+        }
 
-            public int valueOf(int x) {
-                x %= m;
-                if (x < 0) {
-                    x += m;
-                }
-                return x;
-            }
-
-            public int valueOf(long x) {
-                x %= m;
-                if (x < 0) {
-                    x += m;
-                }
-                return (int) x;
-            }
-
-            public int mul(int x, int y) {
-                return valueOf((long) x * y);
-            }
-
-            public int mul(long x, long y) {
-                x = valueOf(x);
-                y = valueOf(y);
-                return valueOf(x * y);
-            }
-
-            public int plus(int x, int y) {
-                return valueOf(x + y);
-            }
-
-            public int plus(long x, long y) {
-                x = valueOf(x);
-                y = valueOf(y);
-                return valueOf(x + y);
-            }
-
-            @Override
-            public String toString() {
-                return "mod " + m;
+        /**
+         * 将A[i]更新为A[i]+mod
+         */
+        public void update(int i, int mod) {
+            for (; i <= n; i += i & -i) {
+                data[i] += mod;
             }
         }
 
         /**
-         * Bit operations
+         * 将A全部清0
          */
-        public static class BitOperator {
-            public int bitAt(int x, int i) {
-                return (x >> i) & 1;
-            }
-
-            public int bitAt(long x, int i) {
-                return (int) ((x >> i) & 1);
-            }
-
-            public int setBit(int x, int i, boolean v) {
-                if (v) {
-                    x |= 1 << i;
-                } else {
-                    x &= ~(1 << i);
-                }
-                return x;
-            }
-
-            public long setBit(long x, int i, boolean v) {
-                if (v) {
-                    x |= 1L << i;
-                } else {
-                    x &= ~(1L << i);
-                }
-                return x;
-            }
-
-            /**
-             * Determine whether x is subset of y
-             */
-            public boolean subset(long x, long y) {
-                return intersect(x, y) == x;
-            }
-
-            /**
-             * Merge two set
-             */
-            public long merge(long x, long y) {
-                return x | y;
-            }
-
-            public long intersect(long x, long y) {
-                return x & y;
-            }
-
-            public long differ(long x, long y) {
-                return x - intersect(x, y);
-            }
+        public void clear() {
+            Arrays.fill(data, 0);
         }
 
-        /**
-         * Power operations
-         */
-        public static class Power {
-            final Modular modular;
-
-            public Power(Modular modular) {
-                this.modular = modular;
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 1; i <= n; i++) {
+                builder.append(query(i) - query(i - 1)).append(' ');
             }
+            return builder.toString();
+        }
+    }
 
-            public int pow(int x, long n) {
-                if (n == 0) {
-                    return 1;
-                }
-                long r = pow(x, n >> 1);
-                r = modular.valueOf(r * r);
-                if ((n & 1) == 1) {
-                    r = modular.valueOf(r * x);
-                }
-                return (int) r;
-            }
 
-            public int inverse(int x) {
-                return pow(x, modular.m - 2);
-            }
+    /**
+     * Mod operations
+     */
+    public static class Modular {
+        int m;
 
-            public int pow2(int x) {
-                return x * x;
-            }
-
-            public long pow2(long x) {
-                return x * x;
-            }
-
-            public double pow2(double x) {
-                return x * x;
-            }
+        public Modular(int m) {
+            this.m = m;
         }
 
-        /**
-         * Log operations
-         */
-        public static class Log2 {
-            public int ceilLog(int x) {
-                return 32 - Integer.numberOfLeadingZeros(x - 1);
+        public int valueOf(int x) {
+            x %= m;
+            if (x < 0) {
+                x += m;
             }
-
-            public int floorLog(int x) {
-                return 31 - Integer.numberOfLeadingZeros(x);
-            }
-
-            public int ceilLog(long x) {
-                return 64 - Long.numberOfLeadingZeros(x - 1);
-            }
-
-            public int floorLog(long x) {
-                return 63 - Long.numberOfLeadingZeros(x);
-            }
+            return x;
         }
 
-        /**
-         * Test whether a number is primes
-         */
-        public static class MillerRabin {
-            Modular modular;
-            Power power;
-
-            /**
-             * Check whether n is a prime s times
-             */
-            public boolean mr(int n, int s) {
-                if (n == 2) {
-                    return true;
-                }
-                if (n % 2 == 0) {
-                    return false;
-                }
-                modular = new Modular(n);
-                power = new Power(modular);
-                for (int i = 0; i < s; i++) {
-                    int x = RANDOM.nextInt(n - 2) + 2;
-                    if (!mr0(x, n)) {
-                        return false;
-                    }
-                }
-                return true;
+        public int valueOf(long x) {
+            x %= m;
+            if (x < 0) {
+                x += m;
             }
-
-            private boolean mr0(int x, int n) {
-                int exp = n - 1;
-                while (true) {
-                    int y = power.pow(x, exp);
-                    if (y != 1 && y != n - 1) {
-                        return false;
-                    }
-                    if (y != 1 || exp % 2 == 1) {
-                        break;
-                    }
-                    exp = exp / 2;
-                }
-                return true;
-            }
+            return (int) x;
         }
 
-        /**
-         * Find all factors of a number
-         */
-        public static class PollardRho {
-            MillerRabin mr = new MillerRabin();
-            Gcd gcd = new Gcd();
-            Random random = new Random();
-
-            public int findFactor(int n) {
-                if (mr.mr(n, 10)) {
-                    return n;
-                }
-                while (true) {
-                    int f = findFactor0(random.nextInt(n), random.nextInt(n), n);
-                    if (f != -1) {
-                        return f;
-                    }
-                }
-            }
-
-            /**
-             * Find all prime factor of n
-             * <br>
-             * p1 => p1^c1
-             * <br>
-             * ...
-             * <br>
-             * pk => pk^ck
-             */
-            public Map<Integer, Integer> findAllFactors(int n) {
-                Map<Integer, Integer> map = new HashMap();
-                findAllFactors(map, n);
-                return map;
-            }
-
-            private void findAllFactors(Map<Integer, Integer> map, int n) {
-                if (n == 1) {
-                    return;
-                }
-                int f = findFactor(n);
-                if (f == n) {
-                    Integer value = map.get(f);
-                    if (value == null) {
-                        value = 1;
-                    }
-                    map.put(f, value * f);
-                    return;
-                }
-                findAllFactors(map, f);
-                findAllFactors(map, n / f);
-            }
-
-            private int findFactor0(int x, int c, int n) {
-                int xi = x;
-                int xj = x;
-                int j = 2;
-                int i = 1;
-                while (i < n) {
-                    i++;
-                    xi = (int) ((long) xi * xi + c) % n;
-                    int g = gcd.gcd(n, Math.abs(xi - xj));
-                    if (g != 1 && g != n) {
-                        return g;
-                    }
-                    if (i == j) {
-                        j = j << 1;
-                        xj = xi;
-                    }
-                }
-                return -1;
-            }
+        public int mul(int x, int y) {
+            return valueOf((long) x * y);
         }
 
-        public static class QuadraticResidue {
-            final Modular modular;
-            final BitOperator bitOperator = new BitOperator();
-            Power power;
-            final PollardRho rho = new PollardRho();
+        public int mul(long x, long y) {
+            x = valueOf(x);
+            y = valueOf(y);
+            return valueOf(x * y);
+        }
 
+        public int plus(int x, int y) {
+            return valueOf(x + y);
+        }
 
-            public QuadraticResidue(Modular modular) {
-                this.modular = modular;
-                power = new Power(modular);
-            }
+        public int plus(long x, long y) {
+            x = valueOf(x);
+            y = valueOf(y);
+            return valueOf(x + y);
+        }
 
-            /**
-             * return \sqrt{n} or -1 if it doesn't exist
-             */
-            public int square(int n) {
-                n = modular.valueOf(n);
-                if (n == 0) {
-                    return 0;
-                }
-                int p = modular.m;
-                if (power.pow(n, (p - 1) / 2) != 1) {
-                    return -1;
-                }
-                while (true) {
-                    int a = RANDOM.nextInt(p);
-                    int w = modular.plus(modular.mul(a, a), -n);
-                    if (power.pow(w, (p - 1) / 2) == 1) {
-                        continue;
-                    }
-
-
-                    int pow = (p + 1) / 2;
-                    int i = 31 - Integer.numberOfLeadingZeros(pow);
-                    int real = 1;
-                    int img = 0;
-                    for (; i >= 0; i--) {
-                        int nReal = modular.plus(modular.mul(real, real),
-                                modular.mul(modular.mul(img, img), w));
-                        int nImg = modular.mul(modular.mul(real, img), 2);
-                        real = nReal;
-                        img = nImg;
-                        if (bitOperator.bitAt(pow, i) == 1) {
-                            nReal = modular.plus(modular.mul(real, a), modular.mul(img, w));
-                            nImg = modular.plus(modular.mul(img, a), real);
-                            real = nReal;
-                            img = nImg;
-                        }
-                    }
-
-                    return real;
-                }
-            }
-
-            public int minPrimitiveRoot() {
-                if (modular.m == 2) {
-                    return 1;
-                }
-                Map<Integer, Integer> factorMap = rho.findAllFactors(modular.m - 1);
-                int[] factors = factorMap.keySet().stream().mapToInt(Integer::intValue).toArray();
-                for (int i = 2; ; i++) {
-                    boolean valid = true;
-                    for (int factor : factors) {
-                        if (power.pow(i, (modular.m - 1) / factor) == 1) {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    if (valid) {
-                        return i;
-                    }
-                }
-            }
+        @Override
+        public String toString() {
+            return "mod " + m;
         }
     }
 
