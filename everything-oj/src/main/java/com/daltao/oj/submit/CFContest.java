c5620b2c296c721ff70a36fa7e5edb7ccd675112
taodaling <taodaling@gmail.com>
2019-09-24 00:31:42 +0800

    atcoder

--- everything-oj/src/main/java/com/daltao/oj/submit/CFContest.java
+++ everything-oj/src/main/java/com/daltao/oj/submit/CFContest.java
@@ -1,6 +1,5 @@
 package com.daltao.oj.submit;
 
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -41,6 +40,8 @@ public class CFContest {
         int inf = (int) 1e8;
         long lInf = (long) 1e18;
         double dInf = 1e50;
+        Modular mod = new Modular((int) 1e9 + 7);
+        Gcd gcd = new Gcd();
 
         public Task(FastIO io, Debug debug) {
             this.io = io;
@@ -52,224 +53,207 @@ public class CFContest {
             solve();
         }
 
+        BitOperator bo = new BitOperator();
 
         public void solve() {
             int n = io.readInt();
-            Segment segment = new Segment(1, n);
-            int[][] xy = new int[n][3];
-            int[] seq = new int[n * 2];
-            int seqTail = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < 2; j++) {
-                    xy[i][j] = io.readInt();
-                }
-                seq[seqTail++] = xy[i][0];
-                seq[seqTail++] = xy[i][1];
+            Node[] nodes = new Node[n + 1];
+            for (int i = 1; i <= n; i++) {
+                nodes[i] = new Node();
+                nodes[i].x = io.readLong();
+                nodes[i].id = i;
             }
 
-            DiscreteMap map = new DiscreteMap(seq, 0, seqTail);
+            for (int i = 1; i < n; i++) {
+                Node a = nodes[io.readInt()];
+                Node b = nodes[io.readInt()];
+                a.next.add(b);
+                b.next.add(a);
+            }
 
+            dfs(nodes[1], null);
+            int ans = 0;
+            for (int i = 1; i <= n; i++) {
+                Node trace = nodes[i];
+                long lastG = trace.x;
+                while (trace != null) {
+                    long g = gcd.gcd(trace.x, lastG);
+                    int d = trace.depth;
+                    for (int j = 19; j >= 0; j--) {
+                        if (trace.ancestors[j] == null) {
+                            continue;
+                        }
+                        if (gcd.gcd(g, trace.gcd[j]) == g) {
+                            trace = trace.ancestors[j];
+                        }
+                    }
+                    ans = mod.plus(ans, mod.mul(d - trace.depth + 1, g));
+                    lastG = g;
+                    trace = trace.ancestors[0];
+                }
+            }
 
+            io.cache.append(ans);
         }
 
 
+        public void dfs(Node root, Node p) {
+            root.next.remove(p);
 
+            root.ancestors[0] = p;
+            root.gcd[0] = gcd.gcd(root.ancestors[0] == null ? 0 : root.ancestors[0].x, root.x);
+            for (int i = 1; i < 20 && root.ancestors[i - 1] != null; i++) {
+                root.ancestors[i] = root.ancestors[i - 1].ancestors[i - 1];
+                root.gcd[i] = gcd.gcd(root.gcd[i - 1], root.ancestors[i - 1].gcd[i - 1]);
+            }
+
+            root.depth = p == null ? 1 : (p.depth + 1);
+
+            for (Node node : root.next) {
+                dfs(node, root);
+            }
+        }
+    }
+
+    public static class Node {
+        List<Node> next = new ArrayList<>(2);
+        Node[] ancestors = new Node[20];
+        long[] gcd = new long[20];
+        int depth;
+        long x;
+        int id;
     }
 
     /**
-     * Created by dalt on 2018/6/1.
+     * Mod operations
      */
-    public static class Randomized {
-        static Random random = new Random();
+    public static class Modular {
+        int m;
 
-        public static double nextDouble(double min, double max) {
-            return random.nextDouble() * (max - min) + min;
+        public Modular(int m) {
+            this.m = m;
         }
 
-        public static void randomizedArray(int[] data, int from, int to) {
-            to--;
-            for (int i = from; i <= to; i++) {
-                int s = nextInt(i, to);
-                int tmp = data[i];
-                data[i] = data[s];
-                data[s] = tmp;
+        public int valueOf(int x) {
+            x %= m;
+            if (x < 0) {
+                x += m;
             }
+            return x;
         }
 
-        public static void randomizedArray(long[] data, int from, int to) {
-            to--;
-            for (int i = from; i <= to; i++) {
-                int s = nextInt(i, to);
-                long tmp = data[i];
-                data[i] = data[s];
-                data[s] = tmp;
+        public int valueOf(long x) {
+            x %= m;
+            if (x < 0) {
+                x += m;
             }
+            return (int) x;
         }
 
-        public static void randomizedArray(double[] data, int from, int to) {
-            to--;
-            for (int i = from; i <= to; i++) {
-                int s = nextInt(i, to);
-                double tmp = data[i];
-                data[i] = data[s];
-                data[s] = tmp;
-            }
+        public int mul(int x, int y) {
+            return valueOf((long) x * y);
         }
 
-        public static void randomizedArray(float[] data, int from, int to) {
-            to--;
-            for (int i = from; i <= to; i++) {
-                int s = nextInt(i, to);
-                float tmp = data[i];
-                data[i] = data[s];
-                data[s] = tmp;
-            }
+        public int mul(long x, long y) {
+            x = valueOf(x);
+            y = valueOf(y);
+            return valueOf(x * y);
         }
 
-        public static <T> void randomizedArray(T[] data, int from, int to) {
-            to--;
-            for (int i = from; i <= to; i++) {
-                int s = nextInt(i, to);
-                T tmp = data[i];
-                data[i] = data[s];
-                data[s] = tmp;
-            }
+        public int plus(int x, int y) {
+            return valueOf(x + y);
         }
 
-        public static int nextInt(int l, int r) {
-            return random.nextInt(r - l + 1) + l;
-        }
-    }
-
-    public static class DiscreteMap {
-        int[] val;
-        int f;
-        int t;
-
-        public DiscreteMap(int[] val, int f, int t) {
-            Randomized.randomizedArray(val, f, t);
-            Arrays.sort(val, f, t);
-            int wpos = f + 1;
-            for (int i = f + 1; i < t; i++) {
-                if (val[i] == val[i - 1]) {
-                    continue;
-                }
-                val[wpos++] = val[i];
-            }
-            this.val = val;
-            this.f = f;
-            this.t = wpos;
+        public int plus(long x, long y) {
+            x = valueOf(x);
+            y = valueOf(y);
+            return valueOf(x + y);
         }
 
-        /**
-         * Return 0, 1, so on
-         */
-        public int rankOf(int x) {
-            return Arrays.binarySearch(val, f, t, x) - f;
+        public int subtract(int x, int y) {
+            return valueOf(x - y);
         }
 
-        public int floorRankOf(int x) {
-            int index = Arrays.binarySearch(val, f, t, x);
-            if (index >= 0) {
-                return index - f;
-            }
-            index = -(index + 1);
-            return index - 1 - f;
-        }
-
-        public int ceilRankOf(int x) {
-            int index = Arrays.binarySearch(val, f, t, x);
-            if (index >= 0) {
-                return index - f;
-            }
-            index = -(index + 1);
-            return index - f;
-        }
-
-        /**
-         * Get the i-th smallest element
-         */
-        public int iThElement(int i) {
-            return val[f + i];
-        }
-
-        public int minRank() {
-            return 0;
-        }
-
-        public int maxRank() {
-            return t - f - 1;
+        public int subtract(long x, long y) {
+            return valueOf(x - y);
         }
 
         @Override
         public String toString() {
-            return Arrays.toString(Arrays.copyOfRange(val, f, t));
+            return "mod " + m;
         }
     }
 
-    public static class Segment implements Cloneable {
-        private Segment left;
-        private Segment right;
-        private long cost;
-        private long prefixSum;
-
-        public void pushUp() {
-            cost = left.cost + right.cost;
-            prefixSum = Math.max(left.cost + right.prefixSum, left.prefixSum);
+    public static class Gcd {
+        public long gcd(long a, long b) {
+            return a >= b ? gcd0(a, b) : gcd0(b, a);
         }
 
-        public void pushDown() {
+        private long gcd0(long a, long b) {
+            return b == 0 ? a : gcd0(b, a % b);
         }
 
+        public int gcd(int a, int b) {
+            return a >= b ? gcd0(a, b) : gcd0(b, a);
+        }
 
-        public Segment(int l, int r) {
-            if (l < r) {
-                int m = (l + r) >> 1;
-                left = new Segment(l, m);
-                right = new Segment(m + 1, r);
-                pushUp();
+        private int gcd0(int a, int b) {
+            return b == 0 ? a : gcd0(b, a % b);
+        }
+    }
+
+    /**
+     * Bit operations
+     */
+    public static class BitOperator {
+        public int bitAt(int x, int i) {
+            return (x >> i) & 1;
+        }
+
+        public int bitAt(long x, int i) {
+            return (int) ((x >> i) & 1);
+        }
+
+        public int setBit(int x, int i, boolean v) {
+            if (v) {
+                x |= 1 << i;
             } else {
-
+                x &= ~(1 << i);
             }
+            return x;
         }
 
-        private boolean covered(int ll, int rr, int l, int r) {
-            return ll <= l && rr >= r;
+        public long setBit(long x, int i, boolean v) {
+            if (v) {
+                x |= 1L << i;
+            } else {
+                x &= ~(1L << i);
+            }
+            return x;
         }
 
-        private boolean noIntersection(int ll, int rr, int l, int r) {
-            return ll > r || rr < l;
+        /**
+         * Determine whether x is subset of y
+         */
+        public boolean subset(long x, long y) {
+            return intersect(x, y) == x;
         }
 
-        public void update(int ll, int rr, int l, int r, int price) {
-            if (noIntersection(ll, rr, l, r)) {
-                return;
-            }
-            if (covered(ll, rr, l, r)) {
-                prefixSum = Math.max(0, cost - price);
-                return;
-            }
-            pushDown();
-            int m = (l + r) >> 1;
-            left.update(ll, rr, l, m, price);
-            right.update(ll, rr, m + 1, r, price);
-            pushUp();
+        /**
+         * Merge two set
+         */
+        public long merge(long x, long y) {
+            return x | y;
         }
 
-        public void query(int ll, int rr, int l, int r) {
-            if (noIntersection(ll, rr, l, r)) {
-                return;
-            }
-            if (covered(ll, rr, l, r)) {
-                return;
-            }
-            pushDown();
-            int m = (l + r) >> 1;
-            left.query(ll, rr, l, m);
-            right.query(ll, rr, m + 1, r);
+        public long intersect(long x, long y) {
+            return x & y;
+        }
+
+        public long differ(long x, long y) {
+            return x - intersect(x, y);
         }
     }
-
 
     public static class FastIO {
         public final StringBuilder cache = new StringBuilder(20 << 20);
